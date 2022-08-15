c610bd8830c5d63e9ef18fb9136c601490095672
0918nobita <nobita.0918@gmail.com>
2022-08-15 13:14:43 +0900

    :white_check_mark: Waxt.Parser のテストを追加した

---
+++ test/Waxt.Parser.Test/Program.fs
@@ -0,0 +1,35 @@
+﻿module Waxt.Parser.Test.Program
+
+open Expecto
+open Thoth.Json.Net
+open VerifyExpecto
+open Waxt.Lexer
+open Waxt.Location
+open Waxt.Parser
+open Waxt.UntypedAst
+
+[<Tests>]
+let parserTest =
+    testTask "parseSExpr" {
+        let (sExpr, rest) =
+            Expect.wantOk
+                (lex "(func foo i32 [x i32 y i32] (i32.mul (i32.add 1 2) 4))")
+                "lexical analysis should succeed"
+            |> ParseSExpr.parseSExpr Pos.origin
+            |> (fun parseResult -> Expect.wantOk parseResult "Parsing to S-expression should succeed")
+
+        Expect.isEmpty rest "Parser should consume all tokens"
+
+        do! Verifier.Verify("parseSExpr", SExpr.toString sExpr)
+
+        let result =
+            ParseUntypedAst.parseStmt sExpr
+            |> (fun parseResult -> Expect.wantOk parseResult "Parsing to Stmt should succeed")
+            |> Stmt.toJson
+            |> Encode.toString 2
+
+        do! Verifier.Verify("parseStmt", result)
+    }
+
+[<EntryPoint>]
+let main argv = runTestsInAssembly defaultConfig argv
