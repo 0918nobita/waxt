0c93bd473f01fb62a9f32c531b1f2f4d1370da7e
0918nobita <nobita.0918@gmail.com>
2022-08-15 22:08:18 +0900

    :sparkles: Waxt.TypedAst ライブラリを追加した

---
+++ src/Waxt.TypedAst/Library.fs
@@ -0,0 +1,62 @@
+[<AutoOpen>]
+module Waxt.TypedAst.Library
+
+open Thoth.Json.Net
+open Waxt.Location
+open Waxt.UntypedAst
+
+type TypedAst =
+    | I32Add of lhs: TypedAst * rhs: TypedAst * at: Range
+    | I32Const of value: int * at: Range
+    | I32Mul of lhs: TypedAst * rhs: TypedAst * at: Range
+    | I32Store of addr: TypedAst * content: TypedAst * at: Range
+    | Var of name: string * ty: Type * at: Range
+
+    interface ILocatable with
+        member this.Locate() =
+            match this with
+            | I32Add (_, _, at)
+            | I32Const (_, at)
+            | I32Mul (_, _, at)
+            | I32Store (_, _, at)
+            | Var (_, _, at) -> at
+
+module TypedAst =
+    let rec toJson: TypedAst -> JsonValue =
+        function
+        | I32Add (lhs, rhs, at) ->
+            Encode.object [ "type", Encode.string "I32Add"
+                            "lhs", toJson lhs
+                            "rhs", toJson rhs
+                            "at", Encode.string (Range.toString at) ]
+
+        | I32Const (value, at) ->
+            Encode.object [ "type", Encode.string "I32Const"
+                            "value", Encode.int value
+                            "at", Encode.string (Range.toString at) ]
+
+        | I32Mul (lhs, rhs, at) ->
+            Encode.object [ "type", Encode.string "I32Mul"
+                            "lhs", toJson lhs
+                            "rhs", toJson rhs
+                            "at", Encode.string (Range.toString at) ]
+
+        | I32Store (addr, content, at) ->
+            Encode.object [ "type", Encode.string "I32Store"
+                            "addr", toJson addr
+                            "content", toJson content
+                            "at", Encode.string (Range.toString at) ]
+
+        | Var (name, ty, at) ->
+            Encode.object [ "type", Encode.string "Var"
+                            "name", Encode.string name
+                            "ty", Type.toJson ty
+                            "at", Encode.string (Range.toString at) ]
+
+    let getType: TypedAst -> Type =
+        function
+        | I32Add _
+        | I32Const _
+        | I32Mul _ -> I32
+        | I32Store _ -> Unit
+        | Var (_, ty, _) -> ty
