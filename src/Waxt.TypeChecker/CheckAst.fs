a33f60791ee9140f59b1a679d9ef7469f5781ad5
Kodai Matsumoto <nobita.0918@gmail.com>
2022-08-17 00:03:01 +0900

    :label: ある比較可能な型の値とインデックスの両方で要素を指定できる Map を定義した

--- src/Waxt.TypeChecker/CheckAst.fs
+++ src/Waxt.TypeChecker/CheckAst.fs
@@ -5,23 +5,31 @@ open Waxt.Type
 open Waxt.TypedAst
 open Waxt.UntypedAst
 
-type FuncSig = private FuncSig of name: string * parameters: list<string * Type> * result: Type
+type IndexedMap<'K, 'V when 'K: comparison> = private IndexedMapInner of values: list<'V> * mapping: Map<'K, int>
 
-type FuncIndex = private FuncIndex of int
+module IndexedMap =
+    let empty<'K, 'V when 'K: comparison> : IndexedMap<'K, 'V> =
+        IndexedMapInner([], Map.empty)
 
-module FuncIndex =
-    let make index = FuncIndex index
+    let add<'K, 'V when 'K: comparison>
+        (key: 'K)
+        (value: 'V)
+        ((IndexedMapInner (values, mapping)): IndexedMap<'K, 'V>)
+        : IndexedMap<'K, 'V> =
+        let index = List.length values
+        let values = values @ [ value ]
+        let mapping = Map.add key index mapping
+        IndexedMapInner(values, mapping)
 
 type FuncName = private FuncName of name: string
 
 module FuncName =
     let make name = FuncName name
 
-type FuncNameIndexMap = private FuncNameIndexMap of Map<FuncName, FuncIndex>
+type FuncSig = private FuncSig of parameters: list<string * Type> * result: Type
 
-type FuncSigCollection = private FuncSigCollection of list<FuncSig>
-
-module FuncSigCollection =
-    let tryGet (FuncIndex index) (FuncSigCollection sigs) = List.tryItem index sigs
+let funcs =
+    IndexedMap.empty<FuncName, FuncSig>
+    |> IndexedMap.add (FuncName.make "foo") (FuncSig([], I32 None))
 
 let checkAst (stmts: list<Stmt>) : list<TypedExpr> = failwith "not implemented"
