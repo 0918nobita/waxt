9c18839c30aedb1aef04321e8bf11343b488b942
0918nobita <nobita.0918@gmail.com>
2022-08-16 22:27:11 +0900

    :label: 関数のシグネチャを保持するための型を定義した

--- src/Waxt.TypeChecker/CheckAst.fs
+++ src/Waxt.TypeChecker/CheckAst.fs
@@ -1,2 +1,27 @@
 [<AutoOpen>]
 module Waxt.TypeChecker.CheckAst
+
+open Waxt.Type
+open Waxt.TypedAst
+open Waxt.UntypedAst
+
+type FuncSig = private FuncSig of name: string * parameters: list<string * Type> * result: Type
+
+type FuncIndex = private FuncIndex of int
+
+module FuncIndex =
+    let make index = FuncIndex index
+
+type FuncName = private FuncName of name: string
+
+module FuncName =
+    let make name = FuncName name
+
+type FuncNameIndexMap = private FuncNameIndexMap of Map<FuncName, FuncIndex>
+
+type FuncSigCollection = private FuncSigCollection of list<FuncSig>
+
+module FuncSigCollection =
+    let tryGet (FuncIndex index) (FuncSigCollection sigs) = List.tryItem index sigs
+
+let checkAst (stmts: list<Stmt>) : list<TypedExpr> = failwith "not implemented"
