15ad083680aedda5a5394de0da56b9e89e087c9a
Kodai Matsumoto <nobita.0918@gmail.com>
2022-08-19 23:15:23 +0900

    :sparkles: uint32 から符号なしLEB128形式へのエンコードを実装した

--- src/Waxt.Wasm/Program.fs
+++ src/Waxt.Wasm/Program.fs
@@ -2,6 +2,32 @@ module Waxt.Wasm
 
 open System.IO
 
+let splitPer7Bits (x: uint32) : list<byte> =
+    if x = 0u then
+        [ 0uy ]
+    else
+        let rec inner (shift: int) =
+            match x >>> (7 * shift) with
+            | 0u -> []
+            | shifted ->
+                uint8 (shifted &&& ~~~ 128u)
+                :: (inner (shift + 1))
+
+        inner 0
+
+let addMsbFlags (bytes: array<byte>) : array<byte> =
+    let lastIndex = Array.length bytes - 1
+
+    bytes
+    |> Array.mapi (fun i byte ->
+        if i = lastIndex then
+            byte
+        else
+            byte ||| 128uy)
+
+let unsignedLeb128 (x: uint32) : array<byte> =
+    x |> splitPer7Bits |> Array.ofList |> addMsbFlags
+
 let writeHeader (writer: BinaryWriter) =
     writer.Write [| 0x00uy
                     0x61uy
@@ -18,6 +44,13 @@ let writeFunctionSection (writer: BinaryWriter) =
     writer.Write [| 0x00uy |] // num function signatures
 
 let () =
+    let data = 471u
+    printfn "       data: %i" data
+    let bytes = splitPer7Bits data
+    printfn "7 bit parts: %A" bytes
+    let leb128 = unsignedLeb128 data
+    printfn "     leb128: %A" leb128
+
     use file = File.Open("out.wasm", FileMode.Create)
     use writer = new BinaryWriter(file)
     writeHeader writer
