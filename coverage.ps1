28c91c1eeccb1f6f678b86989fec4cdbe2562d2e
0918nobita <nobita.0918@gmail.com>
2022-08-28 14:52:22 +0900

    :white_check_mark: Windows PowerShell Core でカバレッジレポートを生成するためのスクリプトを追加した

---
+++ coverage.ps1
@@ -0,0 +1,14 @@
+If (Test-Path test/**/TestResults) {
+    Remove-Item -Recurse -Force test/**/TestResults
+}
+
+if (Test-Path coverage) {
+    Remove-Item -Recurse -Force coverage
+}
+
+dotnet test --collect:"XPlat Code Coverage"
+
+dotnet reportgenerator `
+    -reports:test/**/TestResults/*/coverage.cobertura.xml `
+    -targetdir:coverage `
+    -reporttypes:Html
